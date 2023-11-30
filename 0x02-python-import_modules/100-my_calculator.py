0f0bda6a0b329c1511d12a424201e14cadb420c8
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-11-30 11:07:36 -0800

    Adding 100-my_calculator.py File

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,17 +6,17 @@ from calculator_1 import add, sub, mul, div
 from sys import argv, exit
 
 if __name__ == "__main__":
-    #create a dictionary of the operators
-    operators = {'+' : add, '-' : sub, '*' : mul, '/' : div}
-    #Checks for the number of arguments
+    # Create a dictionary of the operators
+    operators = {'+': add, '-': sub, '*': mul, '/': div}
+    # Checks for the number of arguments
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    #Checks for the operator
+    # Checks for the operator
     operator = argv[2]
     if operator not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    #Performs the operation
+    # Performs the operation
     result = operators[operator](int(argv[1]), int(argv[3]))
     print(f"{argv[1]} {operator} {argv[3]} = {result}")
