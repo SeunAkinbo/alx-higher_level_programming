f2b8cb3c0be5960248d69b2f87f74b7254ef5aa2
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-11-30 10:50:59 -0800

    Adding 100-my_calculator.py File

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,28 +4,16 @@ from calculator_1 import add, sub, mul, div
 from sys import argv, exit
 
 if __name__ == "__main__":
-    argLen = len(argv) - 1
-    operators = ['+', '-', '*', '/']
+    operators = {'+' : add, '-' : sub, '*' : mul, '/' : div}
 
-    if argLen == 3:
-        if argv[2] not in operators:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        for operator in operators:
-            if argv[2]  == operator and operator == operators[0]:
-                print(f"{argv[1]} {argv[2]} {argv[3]} = {add(int(argv[1]), int(argv[3]))}")
-                break
-            elif argv[2] == operator and operator == operators[1]:
-                print(f"{argv[1]} {argv[2]} {argv[3]} = {sub(int(argv[1]), int(argv[3]))}")
-                break
-            elif argv[2] == operator and operator == operators[2]:
-                print(f"{argv[1]} {argv[2]} {argv[3]} = {mul(int(argv[1]), int(argv[3]))}")
-                break
-            elif argv[2] == operator and operator == operators[3]:
-                print(f"{argv[1]} {argv[2]} {argv[3]} = {div(int(argv[1]), int(argv[3]))}")
-                break
-            else:
-                continue
-    else:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
+    operator = argv[2]
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    result = operators[operator](int(argv[1]), int(argv[3]))
+    print(f"{argv[1]} {operator} {argv[3]} = {result}")
