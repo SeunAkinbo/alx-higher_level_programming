e029a2e9d8bfae8d46b5df1ee7576badcf927b51
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-11-30 06:08:28 -0800

    Adding 3-infinite_add.py File

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    length = len(args)
+    total = 0
+    index = 1
+
+    if length == 1:
+        print(total)
+    else:
+        while index <  length:
+            total += int(args[index])
+            index += 1
+        print(total)
