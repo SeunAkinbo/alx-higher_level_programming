3006b3eed018d463e609264f4fffc0cfe6061f5a
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-11-30 10:11:45 -0800

    Adding 102-magic_calculation.py File

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-    result = 0
+
+    # Initialize variables and functions
+    add = None
+    sub = None
+
+    # Import add and sub functions from magic_calculation_102 module
+    import magic_calculation_102
+    from magic_calculation_102 import add, sub
 
     if a < b:
-        add = __import__('magic_calculation_102').add
-        sub = __import__('magic_calculation_102').sub
-        result = add(a, b)
-
-        for i in range(4, 7):
-            result = add(result, i)
-
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
     else:
-        result = __import__('magic_calculation_102').sub(a, b)
-
-    return result
+        return sub(a, b)
 
