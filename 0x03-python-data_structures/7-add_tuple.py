2af61a536fb99a17d2cef0411c73941d6fbf3948
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-01 07:38:10 -0800

    Adding 7-add_tuple.py File

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Ensure both tuples have at least 2 elements
+    tuple_a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
+    tuple_b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
+
+    # Add the corresponding elements of each tuple
+    result_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+    return result_tuple
