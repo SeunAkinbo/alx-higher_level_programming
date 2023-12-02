0b6d0acb722644233cab759d041a6065f7bdf043
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-02 07:58:32 -0800

    Adding 13-is_palindrome.c File

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,22 +1,23 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
+ * reverse_list - reverses a linked list
  * @head: pointer to the first node in the list
  * Return: pointer to the first node in the new list
  */
-void reverse_listint(listint_t **head)
+
+void reverse_list(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *current = *head;
+	listint_t *curr = *head;
 	listint_t *next = NULL;
 
-	while (current)
+	while (curr)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
 	}
 
 	*head = prev;
@@ -51,7 +52,7 @@ int is_palindrome(listint_t **head)
 		slow = slow->next;
 	}
 
-	reverse_listint(&dup);
+	reverse_list(&dup);
 
 	while (dup && temp)
 	{
