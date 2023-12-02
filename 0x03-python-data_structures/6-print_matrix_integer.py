bdd4628b3151eb81e0d3f9420f1b1b0341f17747
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-02 09:57:39 -0800

    Adding 6-print_matrix_integer.py File

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # Looping through the list
-    for row in matrix:
-        # Unpacking the list using the * anad printing to the stdout
-        print(*row)
+    i = 0
+    while i < len(matrix):
+        j = 0
+        while j < len(matrix[i]):
+            if j == len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end='')
+            else:
+                print("{:d}".format(matrix[i][j]), end=' ')
+            j += 1
+        print()
+        i += 1
