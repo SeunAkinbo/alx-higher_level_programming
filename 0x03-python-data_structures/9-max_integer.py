eddc98ecca02700a21a2972d74266ac4690348a9
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-02 09:22:46 -0800

    Adding 9-max_integer.py File

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
         return None
 
     for num in my_list:
-        max_num = -1000
+        max_num = 0
         if num > max_num:
             max_num = num
     return max_num
