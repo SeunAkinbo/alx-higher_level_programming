5bcd8ffc38da972ebfdcccffe750a50d668fd142
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-02 09:25:00 -0800

    Adding 9-max_integer.py File

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 max_integer = __import__('9-max_integer').max_integer
 
-my_list = [-1]
+my_list = [-17, -200, -8]
 max_value = max_integer(my_list)
 print("Max: {}".format(max_value))
 
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    max_num = -1000
     if not my_list:
         return None
 
     for num in my_list:
-        max_num = 0
         if num > max_num:
             max_num = num
     return max_num
