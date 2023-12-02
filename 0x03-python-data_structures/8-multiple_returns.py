6f8eec03feac65365c2c7eeb4b4f46d1480b6233
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-02 09:45:15 -0800

    Adding 8-multiple_returns.py File

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    char = sentence[0] if sentence[0] != '' else None
+    char = sentence[0] if len(sentence) != 0 else None
     return (len(sentence), char)
