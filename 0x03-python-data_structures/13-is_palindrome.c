8a2ecc89a9855aa1500c48288e5a513585bdf7b4
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-02 07:39:42 -0800

    Adding 13-is_palindrome.c File

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,75 +1,52 @@
 #include "lists.h"
-#include <stdbool.h>
 
 /**
- * reverse_list - Reverses a linked list
- * @head: Linked list
- * Return: returs reversed linked list
- **/
-
-listint_t *reverse_list(listint_t *head)
-{
-	listint_t *current = head;
-	listint_t *before;
-	listint_t *after;
-
-	if (current == NULL)
-		return (NULL);
-
-	while (current != NULL)
-	{
-		after = current->next;
-		current->next = before;
-		before = current;
-		current = after;
-	}
-	return (before);
-}
-
-
-/**
- * is_palindrome - Checks a linked list for palindrome
- * @head: Linked list head
- * Return: 1 for success 0 otherwise
- **/
-
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to pointer of first node of the list
+ * Return: 1 if the list is a palindrome, 0 otherwise
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *prev_slow = *head, *middle = NULL;
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL, *temp;
 
-	if (*head == NULL)
-		return (1);
+    // Find the middle node using the slow-fast pointer technique
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
 
-	while (fast && fast->next)
-	{
-		fast = fast->next->next;
-		prev_slow = slow;
-		slow = slow->next;
-	}
-	if (fast != NULL)
-	{
-		middle = slow;
-		slow = slow->next;
-	}
-	slow = reverse_list(slow);
-	fast = *head;
-	while (slow != NULL)
-	{
-		if (fast->n != slow->n)
-		{
-			return (0);
-		}
-		fast = fast->next;
-		slow = slow->next;
-	}
-	slow = reverse_list(slow);
-	if (middle != NULL)
-	{
-		prev_slow->next = middle;
-		middle->next = slow;
-	}
-	else
-		prev_slow->next = slow;
+    // Reverse the first half of the list
+    while (slow) {
+        temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
+    }
 
-	return (1);
+    // Compare the reversed first half with the second half
+    slow = prev;
+    while (prev) {
+        if (prev->n != slow->n) {
+            return 0;
+        }
+        prev = prev->next;
+        slow = slow->next;
+    }
+
+    // Restore the original list
+    slow = prev;
+    prev = NULL;
+    while (slow) {
+        temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
+    }
+
+    *head = prev;
+
+    return 1;
 }
+
