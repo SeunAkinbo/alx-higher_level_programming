c80997db07905431559517f84aedcb515ac2e747
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-06 03:49:11 -0800

    Adding 8-simple_delete.py File

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    return {k: v for k, v in a_dictionary.items() if k != key}
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
+    # return {k: v for k, v in a_dictionary.items() if k != key}
