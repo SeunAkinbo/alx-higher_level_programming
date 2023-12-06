3dd304a3b3cb43de26f659ea45b9eb057a485461
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-06 04:00:25 -0800

    Adding 1-search_replace.py File

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    my_list = list(map(lambda x: 89 if x == 2 else x, my_list))
-    return my_list
+    # return list(map(lambda x: 89 if x == 2 else x, my_list))
+    def update(item):
+        return item if item != search else replace
+    return list(map(update, my_list))
