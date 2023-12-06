60576e7a7416e7cac5f685ab45ae6418350262d7
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-06 01:44:18 -0800

    Updating 0-square_matrix_simple.py File

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda row: list(map(lambda x:
-                                               x ** 2, row)), matrix))
-    return new_matrix
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
