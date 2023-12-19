3bd6f78e9df081588aedb1b92df11d3356815451
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-19 13:39:43 -0800

    Adding File: 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 4-square.py by Oluwaseun Akinbo
+
+"""A class Square File"""
+
+
+class Square:
+    """Defines a class Square"""
+    def __init__(self, size=0):
+        """__init__ Initializing the class object with attribute.
+
+            Args:
+            _size: A private instance attribute
+        """
+        self.__size = size
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def size(self):
+        return self.__size
