d974c981dbc691c884bb0f404c04d34c022896db
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-19 12:35:02 -0800

    Adding File: 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-# 0-square.py by Oluwaseun Akinbo
+# 1-square.py by Oluwaseun Akinbo
 
 """A class Square File"""
 
 
 class Square:
     """Defines a class Square"""
-    def __init__(self, size):
-        """Initializing the class object with attributes"""
-        self._size = size
+    def __init__(self, _size):
+        """Initializing the class object with attribute
+            __init__
+            _size: A private instance attribute
+        """
+
+        self._size = _size
