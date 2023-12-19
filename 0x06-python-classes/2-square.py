d0e4b4c160c500e9443dd24f206e427161d45de9
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-19 12:52:53 -0800

    Adding file: 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 1-square.py by Oluwaseun Akinbo
+
+"""A class Square File"""
+
+
+class Square:
+    """Defines a class Square"""
+    def __init__(self, size=0):
+        """__init__ Initializing the class object with attribute.
+
+            Args:
+            _size: A private instance attribute
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
