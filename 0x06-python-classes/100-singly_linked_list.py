3f6ff2739d7b00000a198db267781c31526e9ea1
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-19 18:11:25 -0800

    Adding File: 100-singly_linked_list.py

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+# 100-singly_linked_list.py by Oluwaseun Akinbo
+
+
+"""
+Defines a node of a singly linked list
+"""
+
+
+class Node:
+    def __init__(self, data, next_node=None):
+        """
+        Initializes the object class and attributes
+        Args:
+            data: The data stored in the node
+            next_: The next node in the list (default: None)
+        """
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        """
+        Retuns the data stored in the node.
+        """
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """
+        Sets the data stored in the node, validating its type for safety.
+        """
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """
+        Returns the next node in the list (None if tail).
+        """
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """ Sets the next node in the list, ensuring it's a Node or None. """
+        if isinstance(value, Node) or value is None:
+            self.__next_node = value
+        else:
+            raise TypeError("next_node must be a Node object")
+
+
+class SinglyLinkedList:
+    """Represents a singly linked list."""
+    def __init__(self):
+        self.__head = None
+
+    def __str__(self):
+        current = self.__head
+        output = ""
+        while current:
+            output += f"{current.data}\n"
+            current = current.next_node
+        return output[:-1]
+
+    def sorted_insert(self, value):
+        """Inserts a new node into the list in ascending order."""
+        new_node = Node(value)
+        current = self.__head
+        previous = None
+
+        """Find the appropriate position to insert the node."""
+        while current and current.data < value:
+            previous = current
+            current = current.next_node
+
+        """Insert the new node before or after the current node."""
+        if previous:
+            previous.next_node = new_node
+        else:
+            self.__head = new_node
+        new_node.next_node = current
