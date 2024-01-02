a13abce989424ffc90230eace9407439caae9668
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-02 14:51:59 -0800

    Adding Updated Files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 The module computes two integers
->>> add_integer(12, 108)
-120
+Module: 0-add_integer
+Function: add_integer
 """
 
 
 def add_integer(a, b=98):
     """The function adds 2 integers
-       Args: a and b
+       Parameters: a and b=98
     """
     if not isinstance(a, (int, float)):
         """Checks if variable a is an integer or float"""
