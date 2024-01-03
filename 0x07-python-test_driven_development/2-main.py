ed720709b6446c6dd8f17119752e8a369f803803
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-03 07:59:39 -0800

    Adding matrix_divided test file

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -2,14 +2,14 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 '''
 matrix = [
-    [1, 2, "5"],
+    [1, 2],
     [4, 5, 6]
 ]
 '''
-
-matrix = {{1, 2, 3}, {4, 5, 6}}
+matrix = [[1, 2, 3], [4, 5, "6"]]
+#matrix = ((1, 2, 3), (4, 5, 6))
 try:
-    print(matrix_divided(matrix, 3))
+    print(matrix_divided(matrix, 2))
     print(matrix)
 except Exception as e:
     print(e)
