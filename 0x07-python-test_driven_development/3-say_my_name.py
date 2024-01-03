5fc82d5d6ade50ea52da130bd94468ada6fc1e4d
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-03 09:17:07 -0800

    Adding 3-say_my_name Files

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+'''
+The Module contains a function that prints the fullname
+Module: 3-say_my_name
+Function: say_my_name
+'''
+
+
+def say_my_name(first_name, last_name=""):
+    '''Prints the <first name> <last name>
+    Parameter: first_name and last_name=""
+    '''
+
+    '''Validates firstname'''
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    
+    '''Validaytes lastname'''
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    '''Prints the fullname'''
+    print(f"My name is {first_name} {last_name}")
