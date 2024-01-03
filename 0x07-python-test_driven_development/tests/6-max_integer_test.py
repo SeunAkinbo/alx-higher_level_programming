8d16b12bd1e4a29e1625aae5866fafeeefa16e7f
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-03 14:56:54 -0800

    Adding File 6-max_integer

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''Unit test for max_integer
+'''
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    '''Class test for max_integer
+    inherits - unittest.TestCase
+    '''
+    def test_max_integer(self):
+        '''Function test for max integer'''
+        param = [1, 5, 9, 12, 11, 56]
+        result = max_integer(param)
+        self.assertEqual(result, 56)
