9b3da11a7e438e5cdedccf1199580a3f09bf929a
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-04 11:35:28 -0800

    Adding File: 101-nqueens.py

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+'''Solving the N queens puzzle
+'''
+
+import sys
+
+
+def is_safe(board, row, col, n):
+    '''Check if there is a queen in the same column'''
+    for i in range(row):
+        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
+            return False
+    return True
+
+def print_solution(board, n):
+    for i in range(n):
+        print("[" + str(i) + ", " + str(board[i]) + "]", end=" ")
+    print()
+
+def solve_nqueens(board, row, n):
+    if row == n:
+        print_solution(board, n)
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row] = col
+            solve_nqueens(board, row + 1, n)
+
+def nqueens(n):
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * n
+    solve_nqueens(board, 0, n)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+        nqueens(n)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
