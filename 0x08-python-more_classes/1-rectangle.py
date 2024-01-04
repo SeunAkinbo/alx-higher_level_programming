e223181d69b0b20208d056675d8fb0daa98c0c0d
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-04 04:30:04 -0800

    Adding file: 1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,12 +12,15 @@ class Rectangle:
         Args:
             - width: A positive integer initialized to 0
             - heigth: A positive integer initialized to 0
-        
+        '''
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        '''
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
