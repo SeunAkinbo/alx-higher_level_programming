a28e4952c4f50c2d0a32612cc8862643112f4edd
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-05 07:02:03 -0800

    Adding File: 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
+'''101-locked_class module'''
+
 class LockedClass:
     '''Prevents dynamic attribute creation except for first_name'''
+    
     __slots__ = ["first_name"]
-
-    def __setattr__(self, name, value):
-        '''Restricts attribute setting'''
-        if name != "first_name":
-            raise AttributeError(f"'LockedClass' object has no "
-                                 f"attribute '{name}'")
-        super().__setattr__(name, value)
