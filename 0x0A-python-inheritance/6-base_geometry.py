d46216314375c94de62547aef6d51dce60a3b221
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-08 12:48:11 -0800

    Adding File: 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module: 5-base_geometry'''
+
+
+class BaseGeometry:
+    '''An empty class BaseGeometry'''
+
+    def area(self):
+        '''A method that computes the area of a geometry'''
+        raise Exception("area() is not implemented")
