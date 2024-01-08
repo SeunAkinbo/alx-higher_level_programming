3d031494516f1536913bcaab5a4ba70dbf4028df
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-08 15:24:47 -0800

    Adding File: 10-square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Module: 10-square.py'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''A Square class that inherit from class Rectangle'''
+
+    def __init__(self, size):
+        '''Initialize the class object
+        Args:
+            - size (integer)
+        '''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''An area method returns the area of the square'''
+        return self.__size ** 2
