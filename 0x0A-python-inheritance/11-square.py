a5c2484111b0061e3ae390c9aa6dccc03673bb5f
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-08 15:32:56 -0800

    Adding File: 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,3 +15,11 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
+
+    def area(self):
+        '''An area method returns the area of the square'''
+        return self.__size ** 2
+
+    def __str__(self):
+        '''Returns the class Square string representation'''
+        return f"[Square] {self.__size}/{self.__size}"
