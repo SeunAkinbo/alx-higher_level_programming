329d4a431122442c03de1aa2507f4923005af642
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-09 13:52:21 -0800

    Adding File: 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,11 +10,11 @@ def append_after(filename="", search_string="", new_string=""):
         - new_string
     '''
     with open(filename, "r+") as file:
-       lines = file.readlines()
-       file.seek(0)
+        lines = file.readlines()
+        file.seek(0)
 
-       for line in lines:
-           file.write(line)
-           if search_string in line:
-               file.write(new_string)
-       file.truncate()
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
+        file.truncate()
