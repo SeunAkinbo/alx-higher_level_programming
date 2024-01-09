8c4dce6be632541e2ee7192c450f03c347733af4
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-09 12:05:21 -0800

    Adding File: 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,7 +12,7 @@ def pascal_triangle(n):
 
     triangle = [[1]]
 
-    for i in  range(1, n):
+    for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
