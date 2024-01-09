b6580e31fd4f1970378d60997e4cbdabfe962294
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-09 05:35:33 -0800

    Adding File: Write a function that appends a string at the end of a text file (UTF8) and returns the number of characters added:

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Module: 2-append_write.py'''
+
+
+def append_write(filename="", text=""):
+    '''Afunction that appends string to the end of the file
+    and returns the number of characters
+    Args:
+        - filename: The file path
+        - text: String to write to file
+    '''
+    with open(filename, "r+") as file:
+        return file.write(text)
