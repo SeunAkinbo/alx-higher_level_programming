83372f3565182cb6ec0eff07f9100f8b6888ec54
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-09 14:48:32 -0800

    Adding File: 101-stats

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''Module: 101-stats.py'''
+
+import sys
+
+total_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
+
+try:
+    for line in sys.stdin:
+        try:
+            ip, date, request, status_code, file_size = line.split(" ", 4)
+            file_size = int(file_size)
+            total_size += file_size
+            status_codes[int(status_code)] += 1
+            line_count += 1
+
+            if line_count % 10 == 0:
+                print("File size:", total_size)
+                for code in sorted(status_codes):
+                    if status_codes[code] > 0:
+                        print(f"{code}: {status_codes[code]}")
+        except ValueError:
+            pass
+
+except KeyboardInterrupt:
+    print("File size:", total_size)
+    for code in sorted(status_codes):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
