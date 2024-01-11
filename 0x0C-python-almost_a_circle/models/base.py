edc6cec804031d3824ba3f7e1f907d0ce924f425
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-11 10:09:28 -0800

    Adding Base Files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Module: base.py'''
+
+
+class Base:
+    '''Base Class
+    - __nb_objects: Private class attribute
+    '''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''Initialize the class object'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_object += 1
+        self.id = Base.__nb_objects
