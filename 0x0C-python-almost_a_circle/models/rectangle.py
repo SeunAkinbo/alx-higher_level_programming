8c538f42c5be5a04514720574dd1cfe5e35e0c91
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-11 16:44:56 -0800

    Updating File: rectangle - str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,7 @@ class Rectangle(Base):
         '''Prints the rectangle to the stdout'''
         for i in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - "\
+                f"{self.width}/{self.height}"
