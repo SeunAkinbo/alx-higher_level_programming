10666ac83983ee6de5f20324cd5199e1774e4491
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-12 17:26:26 -0800

    Updating base - json_to_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,7 +22,24 @@ class Base:
     def to_json_string(list_dictionaries):
         '''Static method that returns a json string
         of the dictionary attributes
+        Args:
+            - list_dictionaries: Dictionary of class attributes
         '''
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Saves the JSON string representation to a file
+        Args:
+            - list_objs: Dictionary of class attributes
+        '''
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            # Writing to a JSON file
+            json_str = cls.to_json_string(
+                    [obj.to_dictionary() for obj in list_objs])
+            file.write(json_str)
