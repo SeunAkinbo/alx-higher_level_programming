0be57c1b3b2bfcef78b36bb6a61df70ff5c39d01
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-12 16:35:17 -0800

    Updating base - json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''Module: base'''
 
+import json
+
 
 class Base:
     '''Base Class
@@ -15,3 +17,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Static method that returns a json string
+        of the dictionary attributes
+        '''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
