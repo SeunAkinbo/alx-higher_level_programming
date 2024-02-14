d9559d981e204c0ea09673302e2d154bb1de478f
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-14 14:27:56 -0800

    Adding File - 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Script that lists all shows
 SELECT show.title, genre.genre_id
 FROM tv_shows show, tv_show_genres genere
-WHERE show.id = genre.show_id
+JOIN genre ON show.id = genre.show_id
 ORDER BY show.title ASC, genre.genre_id ASC;
