29d796101d2b6a66cde0e504e52f246d863d4574
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-13 19:11:03 -0800

    Update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,5 +6,5 @@ CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER DATABASE hbtn_0c_0
+ALTER IF EXISTS DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
