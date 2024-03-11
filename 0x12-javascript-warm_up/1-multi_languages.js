da859b46e689a930a5d564f7f61999781053560e
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-11 04:27:15 -0700

    Updating Styling

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let myVar = "C is fun";
+let myVar = 'C is fun';
 console.log(myVar);
-myVar = "Python is cool";
+myVar = 'Python is cool';
 console.log(myVar);
-myVar = "Javascript is amazing";
+myVar = 'Javascript is amazing';
 console.log(myVar);
