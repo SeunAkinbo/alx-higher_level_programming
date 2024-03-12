8e9c269ae1b759b372ed30af2eca1fa1da9f51cd
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-12 16:07:18 -0700

    Updating 102-concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,6 +4,6 @@ const fileSystem = require('fs');
 
 const filePath = process.argv.slice(2);
 
-const fileDataA = fileSystem.readFile(filePath[0], 'utf8');
-const fileDataB = fileSystem.readFile(filePath[1], 'utf8');
-fileSystem.writeFile(filePath[2], fileDataA + fileDataB);
+const fileDataA = fileSystem.readFileSync(filePath[0], 'utf8');
+const fileDataB = fileSystem.readFileSync(filePath[1], 'utf8');
+fileSystem.writeFileSync(filePath[2], fileDataA + fileDataB);
