9b53ba09881fb52209d1635a0eebe33176e03be7
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-12 08:21:05 -0700

    Updating 2-rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || w === undefined || h <= 0 || h === undefined) {
-      this.width = undefined;
-      this.height = undefined;
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
