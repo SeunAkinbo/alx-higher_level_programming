7936e860139631cf8afe835414ba2e83737a1c4d
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-12 15:23:55 -0700

    Adding File 102-concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,15 +1,42 @@
 #!/usr/bin/node
 
-const pathList = process.argv.slice(2);
-
 const fs = require('fs');
 
-const fpA = pathList[0];
-const fpB = pathList[1];
-const dpC = pathList[2];
+function errHandler(err) {
+  if (err) {
+    console.error("Error:", err);
+    process.exit(1);
+  }
+}
 
-const fileACont = fs.readFileSync(fpA, 'utf8');
-const fileBCont = fs.readFileSync(fpB, 'utf8');
-const concat = fileACont + '\n' + fileBCont;
+const pathList = process.argv.slice(2);
+if (pathList.length < 3) {
+  console.error("Usage: ./102-concat fileA fileB fileC");
+  process.exit(1);
+}
 
-fs.writeFileSync(dpC, concat);
+const filePathA = pathList[0];
+const filePathB = pathList[1];
+const destPathC = pathList[2];
+
+fs.readFile(filePathA, 'utf8', (err, dataA) => {
+  errHandler(err);
+  
+  fs.readFile(filePathB, 'utf8', (err, dataB) => {
+    errHandler(err);
+    
+    const concatData = `${dataA}\n${dataB}`;
+    fs.writeFile(destPathC, concatData, (err) => {
+      handleError(err);
+    });
+  });
+});
+
+
+
+/*
+const fileACont = fs.readFileSync(filePathA, 'utf8');
+const fileBCont = fs.readFileSync(filePathB, 'utf8');
+const concatData = fileACont + '\n' + fileBCont;
+fs.writeFileSync(destPathC, concatData);
+*/
