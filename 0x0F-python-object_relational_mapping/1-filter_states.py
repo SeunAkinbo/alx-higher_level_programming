491b50d44278aa505b145cb018015070fc046921
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-14 19:29:23 -0700

    Updating 1-filter_states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''Module 0-select_states'''
+
+import MySQLdb
+import sys
+
+
+def states(username, password, name):
+    '''The script lists all states from the database hbtn_0e_0_usa
+        Args - sql username, sql password, database name
+    '''
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=name)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    ''' The main name check '''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    states(username, password, database)
