a4b24868f065f01f93a0b0664e619fe931021dcf
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-15 05:07:16 -0700

    Updating my_safe_filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -31,7 +31,8 @@ def states(username, password, name, searched):
 if __name__ == "__main__":
     ''' The main name check '''
     if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
+        print("Usage: python script.py <username> <password>\
+                <database> <state_name>")
         sys.exit(1)
 
     username = sys.argv[1]
