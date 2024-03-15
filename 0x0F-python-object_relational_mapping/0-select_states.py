faaf3f4f6f53d82f37a647ef38b72ba0b12d4781
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-14 18:18:32 -0700

    Adding File select_states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''Module 0-select_states'''
+
+import MySQLdb
+import sys
+
+
+def states(username, password, name):
+    '''The script lists all states from the database hbtn_0e_0_usa'''
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=name)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    '''The main name check'''
+    if len(sys.argv) != 4:
+        print("Usage: ./<file_name> <username> <password> <database>")
+        sys.exit(1)
+
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys,argv[3]
+
+        states(username, password, database)
