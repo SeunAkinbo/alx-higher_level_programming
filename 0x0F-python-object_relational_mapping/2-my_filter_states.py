59a3efe77f4b088959f5916b99f61b6156fe98fd
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-15 05:44:02 -0700

    Updating 2-my_filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''Module 0-select_states'''
+'''Module 2-my_filter_states'''
 
 import MySQLdb
 import sys
 
 
-def states(username, password, name, searched):
+def states(username, password, name, state_name):
     '''The script lists all states from the database hbtn_0e_0_usa
-        Args - username, password, name, searched
+        Args - username, password, name, state_name
     '''
     db = MySQLdb.connect(
             host="localhost",
@@ -17,7 +17,8 @@ def states(username, password, name, searched):
             db=name)
 
     cur = db.cursor()
-    query = f"SELECT * FROM states WHERE name = '{searched}' ORDER BY id ASC"
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+            state_name)
     cur.execute(query)
 
     rows = cur.fetchall()
@@ -38,5 +39,5 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    searched = sys.argv[4]
-    states(username, password, database, searched)
+    state_name = sys.argv[4]
+    states(username, password, database, state_name)
