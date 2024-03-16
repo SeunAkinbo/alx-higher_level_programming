78d4b015d61bef72bcf1d612c8815b01efd635fc
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-16 12:36:44 -0700

    Updating 13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Module - 13-model_state_delete_a"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+def delete_states(username, password, db_name):
+    """Function deletes all states in the database
+    Args:
+        -username
+        -password
+        -db_name
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            username, password, db_name), pool_pre_ping=True)
+
+    Base.metadata.bind = engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).all()
+
+    if states:
+        for state in states:
+            if 'a' in state:
+                session.delete(state)
+
+    session.commit()
+    session.close
+
+
+if __name__ == "__main__":
+    """main checker"""
+    if len(sys.argv) != 4:
+        print("Usage: python <username> <password> <database>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    delete_states(username, password, db_name)
