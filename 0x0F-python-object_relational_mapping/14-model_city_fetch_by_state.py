1e0e7d6f8a6b12526cf578dc5b81999a795b89f1
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-17 04:04:27 -0700

    Update 14-model

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,45 +1,40 @@
 #!/usr/bin/python3
-
-"""
-Script that prints all City objects from the database hbtn_0e_14_usa.
-Parameters for script: mysql username, mysql password, database name.
-Must use the `SQLAlchemy` module.
-Must import `State` and `Base` from `model_state` -
-`from model_state import Base, State`
-Script should connect to a MySQL server runnimg on `localhost` at port `3306`
-Results must be sorted in ascending order by `cities.id`
-Results must be display as they are in the example below -
-(<state name>: (<city id>) <city name>)
-Code should not be executed when imported.
-"""
+"""Module: 14-model_city_fetch_by_state"""
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State, Base
 from model_city import City
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    # Check if the number of arguments is correct
-    if len(argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(argv[0]))
-        exit()
 
-    # Creating the connection string
+def cities(username, password, db_name):
+    """The function prints all the cities in the database"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+                            username, password, db_name), pool_pre_ping=True)
 
-    # Creating an instance of Session
+    Base.metadata.bind = engine
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Querying multiple tables in database and printing info from tables
-    cities = session.query(State.name, City.id, City.name).filter(
-        State.id == City.state_id).order_by(City.id)
+    cities = session.query(State.name, City.id, City.name).join(City,
+                           State.id == City.state_id).order_by(City.id).all()
 
-    # Displaying the results
-    for city in cities:
-        print("{:s}: ({:d}) {:s}".format(city[0], city[1], city[2]))
+    if cities:
+        for state_name, city_id, city_name in cities:
+            print("{}: ({}) {}".format(state_name, city_id, city_name))
 
-    # Closing the session
     session.close()
+
+
+if __name__ == '__main__':
+    """The main function"""
+    if len(sys.argv) != 4:
+        print("Usage: python <script> <username> <password> <db_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    cities(username, password, db_name)
