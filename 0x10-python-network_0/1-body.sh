d9215a5cb6240e84de671483c4951802e5b63d65
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-27 12:40:36 -0700

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # The script takes in the URL, sends a GET request, and displays the body of the response
-curl -s -w "%{http_code}\n" -o /dev/null "$1" | grep -q 200 && curl -s "$1"
+curl -s -L "$1"
