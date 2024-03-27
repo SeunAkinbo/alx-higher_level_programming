d9c48cbc731938d0da734ae75d31ab2622ae044f
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-27 14:45:29 -0700

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # The script makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-
+curl -s -X PUT -d "status=You%20got%20me%21" 0.0.0.0:5000/catch_me
