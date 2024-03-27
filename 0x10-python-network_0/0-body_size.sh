dcb520806cc6730e50f84541c1abb3247554e639
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-27 12:18:58 -0700

    Updating body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # The script takes a URL, sends a request to that URL,and displays the size of the body of the response
-curl -Is <url> | grep -i Content-Length | awk '{print $2}' | tr -d '\r'
+curl -Is "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r'
