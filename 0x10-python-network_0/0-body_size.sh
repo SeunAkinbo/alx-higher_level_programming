2a3908a8dd6de57ca67bb92ffc7b8e97a65fd4fc
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-27 11:36:42 -0700

    Updating body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,6 +2,23 @@
 # The script takes a URL, sends a request to that URL,
 # and displays the size of the body of the response
 
+# Check if a URL argument is provided
+if [ $# -eq 0 ];
+then
+	"Error: Please provide a URL as argument"
+	exit 1
+fi
+
+# Get the URL from the first argument
 URL=$1
 
-curl -Is "$URL" | grep -i Content-Length | awk '{print $2}' | tr -d '\r'
+# Use curl to fetch the response
+RESPONSE_FILE=$(mktemp)
+curl -sSLw "%{size_download}" -o "$RESPONSE_FILE" "$URL" > /dev/null
+
+# Print the response size in byte
+SIZE=$(cat "$RESPONSE_FILE")
+echo $SIZE
+
+# Clean up temporary file
+rm "$RESPONSE_FILE"
