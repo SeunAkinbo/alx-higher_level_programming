cc218fc25ff9f490155c10048fdbb3181628f904
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-28 19:48:46 -0700

    Updating Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module: 8-json_api"""
+
+import requests
+import sys
+
+
+def jsonAPI(url):
+    """"""
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    data = {"q": letter}
+    response = requests.post(url, data=data)
+
+    try:
+        obj = response.json()
+        if obj:
+            return "[{}] {}".format(data['id'], data['name'])
+        else:
+            return "No result"
+    except ValueError:
+        return "Not a valid JSON"
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    res = jsonAPI(url)
+    print(res)
