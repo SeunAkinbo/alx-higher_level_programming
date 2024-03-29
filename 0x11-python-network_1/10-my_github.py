2a79f5ad4780b7be1a25c8af94a15a3bd7569ddf
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-28 20:26:30 -0700

    My GitHub

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module: 10-my_github"""
+
+import requests
+import sys
+
+
+def logMeIn():
+    """The function retrives the user ID with the
+    GitHub credentials provided
+    """
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+    return response.json().get('id')
+
+
+if __name__ == "__main__":
+    res = logMeIn()
+    print(res)
