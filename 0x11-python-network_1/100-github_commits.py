8e0f65cc01194fa4e8adaa54f3deb16a08c01270
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-28 21:04:25 -0700

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module: 100-github_commits"""
+
+import requests
+import sys
+
+
+def getCommits():
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        objs = response.json()
+        x = 0
+        while x < 10:
+            encrypt_id = objs[x].get("sha")
+            author = objs[x].get("commit").get("author").get("name")
+            print("{}: {}".format(encrypt_id, author))
+            x += 1
+
+
+if __name__ == "__main__":
+    res = getCommits()
+    res
