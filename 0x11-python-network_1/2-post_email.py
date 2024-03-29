b1e4df066749a7f8666090e8f039bc7900251e8c
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-28 18:00:06 -0700

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,6 +2,7 @@
 """Module: 2-post_email"""
 
 from urllib.request import Request, urlopen
+from urllib.error import URLError
 from urllib.parse import urlencode
 import sys
 
@@ -10,13 +11,20 @@ def postEmail():
     """The function fetches the url response header and displays value"""
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urlencode({"email": email})
-    data = data.encode('ascii')
-    #req = Request(url, data)
+    data = urlencode({"email": email}).encode('ascii')
+    req = Request(url, data=data, method='POST')
 
-    with urlopen(url, data) as response:
-        post_email = response.read().decode('utf-8')
-        return post_email
+    try:
+        with urlopen(req) as response:
+            post_email = response.read().decode("utf-8")
+            return post_email
+    except URLError as e:
+        if hasattr(e, "reason"):
+            print("Failed to reach server.")
+            print("Reason: ", e.reason)
+        elif hasattr(e, "code"):
+            print("Request execution failed.")
+            print("Error coded: ", e.code)
 
 
 if __name__ == "__main__":
