caf67c2db4050d4be79a5b11b42c33e529faa248
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-23 15:55:29 -0700

    Semistandardization

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,19 +3,19 @@
 
 const fs = require('fs');
 
-function readAndPrintFileContent(filePath) {
-	fs.readFile(filePath, 'utf-8', (err, data) => {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		console.log(data);
-	});
+function readAndPrintFileContent (filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
 }
 
 if (process.argv.length !== 3) {
-	console.log('Usage: node script.js <file_path>');
-	process.exit(1);
+  console.log('Usage: node script.js <file_path>');
+  process.exit(1);
 }
 
 const filePath = process.argv[2];
