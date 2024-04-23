82a41e9e347eb37be519f8beaa35c4863e87935b
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-23 15:50:11 -0700

    Reading File Data

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Reading data from file
+
+const fs = require('fs');
+
+function readAndPrintFileContent(filePath) {
+	fs.readFile(filePath, 'utf-8', (err, data) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		console.log(data);
+	});
+}
+
+if (process.argv.length !== 3) {
+	console.log('Usage: node script.js <file_path>');
+	process.exit(1);
+}
+
+const filePath = process.argv[2];
+readAndPrintFileContent(filePath);
