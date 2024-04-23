ac430eca20b1d77a78904954867a951cf19d54b6
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-23 16:22:57 -0700

    Status Code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Writing content to file
+
+const request = require('request');
+
+function getRequestStatusCode (url) {
+  request.get(url, (err, response) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`code: ${response.statusCode}`);
+  });
+}
+
+if (process.argv.length !== 3) {
+  console.log('Usage: node script.js <url>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+getRequestStatusCode(url);
