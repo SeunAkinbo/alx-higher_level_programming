f2ac017d17a4d99854ff0dda79d6ac83a3bcca73
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-23 19:40:37 -0700

    Updating

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,38 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+const fetchMovieCharacters = (movieId) => {
+  const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const characters = JSON.parse(body).characters;
+      fetchCharactersDetails(characters, 0);
+    } else {
+      console.error('Error fetching movie data:', error || 'Invalid response');
     }
   });
+};
+
+const fetchCharactersDetails = (characterUrls, index) => {
+  if (index >= characterUrls.length) return;
+
+  request(characterUrls[index], (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+      fetchCharactersDetails(characterUrls, index + 1);
+    } else {
+      console.error('Error fetching character data:', error || 'Invalid response');
+    }
+  });
+};
+
+// Check if Movie ID is provided as a command-line argument
+if (process.argv.length !== 3) {
+  console.log('Usage: node script.js <movie_id>');
+  process.exit(1);
 }
+
+const movieId = process.argv[2];
+fetchMovieCharacters(movieId);
