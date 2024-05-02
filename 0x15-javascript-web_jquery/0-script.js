d0e35dd259ac04d293991123c1066f0ec1b262b4
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-05-02 18:03:12 +0000

    Project Files && README

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+/* The Java Script updates the text color of the
+   <header> element to red (#FF0000), using the
+   document.querySelector() method
+*/
+
+document.querySelector("header").style.color = "#FF0000";
