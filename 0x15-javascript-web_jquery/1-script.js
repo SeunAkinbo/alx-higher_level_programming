1a9af7bafa0d444b7743a7666fdbbd55e77631ee
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-05-02 22:49:47 +0000

    Project Solution

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,8 @@
+/* Updates the text color of the <header> element with
+   red (#FF0000), using the JQuery API is mandatory.
+   Use of document.querySelector is not allowed
+*/
+
+$(document).ready(function () {
+  $("header").css("color", "#FF0000");
+});
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,11 @@
+/* The script updates the text color of the <header> element to
+   red (#FF0000) when the user clicks on the tag DIV#red_header:
+   Use of document.querySelector is not allowed
+   You must use the JQuery API
+ */
+
+$(document).ready(() => {
+  $("DIV#red_header").click(() => {
+    $("header").css("color", "#FF0000");
+  });
+});
